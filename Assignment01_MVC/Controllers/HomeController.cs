2e3300d8aa65f7ab04d027cad9e98b8c57b04623
mohamed-eldeghiedy <eldegedym@gmail.com>
2025-09-17 02:18:47 +0300

    S02

---
+++ Assignment01_MVC/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Assignment01_MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            //var result = new ContentResult();
+            //result.Content = "hello from index page ";
+            //result.StatusCode = StatusCodes.Status200OK;
+            //result.ContentType = "text/html";
+            //return result;
+            return View();
+        }
+
+        public RedirectResult Update()
+        {
+            var result = new RedirectResult("https://www.google.com");
+            return result;
+        }
+
+        public RedirectToActionResult Add()
+        {
+            var result = new RedirectToActionResult(nameof(ProductsController.Add), "Products", new { id = 1 });
+            return result;
+
+
+
+        }
+
+        public IActionResult  Privacy()
+        {
+            return View();
+        
+         
+        }
+
+    }
+}
